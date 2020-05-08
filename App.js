4b6673dc30a3954162fa89d8acdf3a3d7ebea377
Ian Lee <ian102423@github.com>
2020-05-08 04:55:09 -0500

    Persist Redux Store

--- App.js
+++ App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 import { YellowBox } from 'react-native'; //disable yellobox
+
 
 YellowBox.ignoreWarnings(['Warning: ...'])
 console.disableYellowBox = true;
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate
+        loading={<Loading />}
+         persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   );
 }
